ca3310a0a1bbbc276b61df62087a66513cdca22b
Kathryn Jones <kathryn.jones@magmared.co.uk>
2024-01-28 16:11:48 +0000

    Inital commit of working image processer. Included sccreenshots of calls to filteredimage endpoint and Elastic Beanstalk deployment. Exported Postman collection (json file) for import for testing endpoint included.

---
+++ README.md
@@ -0,0 +1,43 @@
+# Full Stack Apps on AWS Project
+
+You have been hired as a software engineer to develop an application that will help the FBI find missing people.  The application will upload images to the FBI cloud database hosted in AWS. This will allow the FBI to run facial recognition software on the images to detect a match. You will be developing a NodeJS server and deploying it on AWS Elastic Beanstalk. 
+You will build upon the application we've developed during the lessons in this course. You'll complete a REST API endpoint in a backend service that processes incoming image URLs.
+
+## Getting Started
+
+You can clone this repo to run the project locally, or navigate to the workspace in the Udacity course.
+
+## Project Instructions
+
+To complete this project, you will need to:
+
+* Set up node environment
+* Create a new endpoint in the server.js file
+* Deploying your system
+
+## Testing
+
+Successful URL responses should have a 200 code. Ensure that you include error codes for the scenario that someone uploads something other than an image and for other common errors.
+
+Testing the /filteredimage endpoint:
+
+### Curl
+
+curl --request GET http://aws-image-processing-microservice-dev.us-east-1.elasticbeanstalk.com/filteredimage?image_url=https://www.photos-public-domain.com/wp-content/uploads/2018/05/siamese-tabby-cat-600x600.jpg --output filtered-image.jpg
+
+### In a Browser
+
+http://aws-image-processing-microservice-dev.us-east-1.elasticbeanstalk.com/filteredimage?image_url=https://www.photos-public-domain.com/wp-content/uploads/2018/05/siamese-tabby-cat-600x600.jpg
+
+### Postman
+
+A postman collection is given in the git repository in the postman_collection directory. Screenshots of the collection run are given in the deployment-screenshots directory.
+
+### Domain URL
+
+aws-image-processing-microservice-dev.us-east-1.elasticbeanstalk.com/
+
+
+## License
+
+[License](LICENSE.txt)
---
+++ image-processing-microservice/server.js
@@ -0,0 +1,77 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
+
+
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+    /**************************************************************************** */
+  //! END @TODO1
+
+  app.get("/filteredimage", async (req, res) => {
+      try {
+          const imageUrl = req.query.image_url;
+
+          if (!imageUrl) {
+              const message = "Missing image url. An image url is required.";
+              console.log(message)
+              res.status(400).send(message);
+          } else {
+              console.log('Image URL: ', imageUrl);
+
+              let filteredImagePath = "";
+
+              try {
+                  filteredImagePath = await filterImageFromURL(imageUrl)
+
+                  console.log('Filtered image path: ', filteredImagePath);
+
+                  res.status(200).sendFile(filteredImagePath, function() {
+                    deleteLocalFiles([filteredImagePath]);
+                  } );
+              } catch (error) {
+                  const message = "Bad image url. Image not found or not an image.";
+                  console.log(message);
+                  res.status(404).send(message);
+              }
+          }
+      } catch (error) {
+          console.log("Internal Server error:", error);
+          res.status(500).send("Unexpected error, check logs.")
+      }
+  } );
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async (req, res) => {
+      res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
